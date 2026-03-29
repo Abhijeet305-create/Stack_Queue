149891069ece1ad67bf7665f1e45764babdf7b4c
user121 <256301033@gkv.ac.in>
2026-03-29 17:43:15 +0530

    stacks and queue

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.printStack();
+    }
+}
